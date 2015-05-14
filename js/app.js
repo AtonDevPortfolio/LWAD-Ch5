db7791a76c12d3dc817d2589860f69a5a9954bf8
anton-b117 <anton@maroonspear.com>
2015-05-14 00:00:02 -0400

    Simple AJAX call.  Seems to work with security restrictions in bmoth browsers.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 var main = function () {
 	"use strict";
 
+	console.log("Taily Po.");
 	// getJSON even parses the JSON for us, so we dont need to
 	// call JSON.parse
 	$.getJSON("cards/aceOfSpades.json", function (card) {
-		// Print the card to the console
-		console.log(card);
+		// create an element to hold the card
+		var $cardParagraph = $("<p>");
+
+		// add text to the paragraph element
+		$cardParagraph.text(card.rank + " of " + card.suit);
+
+		// append the card paragraph to main
+		$("main").append($cardParagraph);
 	});
 }
 
