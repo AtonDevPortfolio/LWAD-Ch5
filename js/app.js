ae9af280bcb69d8061af9c7e84d430fec9ab218b
anton-b117 <anton@maroonspear.com>
2015-05-13 23:45:33 -0400

    Calls json in browser.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 var main = function () {
 	"use strict";
 
-	console.log("Hello. Mordor.");
+	// getJSON even parses the JSON for us, so we dont need to
+	// call JSON.parse
+	$.getJSON("cards/aceOfSpades.json", function (card) {
+		// Print the card to the console
+		console.log(card);
+	});
 }
 
 $(document).ready(main);
