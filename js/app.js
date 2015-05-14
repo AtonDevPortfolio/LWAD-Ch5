71a5ed03237a227b569caff2d67904bd479c2e55
anton-b117 <anton@maroonspear.com>
2015-05-14 00:39:16 -0400

    Debugging 2nd AJAX example...

--- js/app.js
+++ js/app.js
@@ -14,6 +14,23 @@ var main = function () {
 		// append the card paragraph to main
 		$("main").append($cardParagraph);
 	});
-}
+
+	$.getJSON("cards/hand.json", function (hand) {
+		var list = $("<ul>");
+
+		// hand is an array, so we can iterate over it
+		// using a forEach loop
+		hand.forEach(function (card) {
+			// create a list item to hold the card
+			// and append it to the list
+			var $card = $("<li>");
+			$card.text(card.rank + " of " + card.suit);
+			$list.append($card);
+		});
+
+		// append the list to the main
+		$("main").append($list);
+	});
+};
 
 $(document).ready(main);
